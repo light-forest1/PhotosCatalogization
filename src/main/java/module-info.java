8debd15af6879c2d08539fafcc8a15fafc45412d
light-forest1 <71901389+light-forest1@users.noreply.github.com>
2020-10-20 22:43:19 +0300

    Add files via upload

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.openjfx.photoscatalogization {
+    requires javafx.controls;
+    exports org.openjfx.photoscatalogization;
+    requires metadata.extractor;
+    requires xmpcore;
+    requires java.desktop;
+    requires java.logging;
+}
